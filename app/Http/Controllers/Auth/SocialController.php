871932b042907bb36f3817c483d2772ea48d9565
Clément Rigo <rigoclement@mydnic.be>
2024-11-08 19:18:01 +0100

    Update SocialController.php

--- app/Http/Controllers/Auth/SocialController.php
+++ app/Http/Controllers/Auth/SocialController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 use Laravel\Socialite\Facades\Socialite;
 
 class SocialController extends Controller
@@ -27,6 +28,7 @@ class SocialController extends Controller
                 'email' => $user->getEmail(),
                 'name' => $user->getName(),
                 'avatar' => $user->getAvatar(),
+                'password' => bcrypt(Str::random()),
             ]
         );
 

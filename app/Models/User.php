17cc5a5920a1073e8bb27b7bae6b1409e32d5902
Clément Rigo <rigoclement@mydnic.be>
2024-11-10 20:19:23 +0100

    feat(user): add avatar and social fields to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,6 +27,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'avatar',
+        'social_provider',
+        'social_provider_id',
     ];
 
     /**

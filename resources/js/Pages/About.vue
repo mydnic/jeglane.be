f6f9d9f0b73ce847e12ed6aa52c8a850c7fe6717
Clément Rigo <rigoclement@mydnic.be>
2024-11-18 19:46:36 +0100

    fix

--- resources/js/Pages/About.vue
+++ resources/js/Pages/About.vue
@@ -66,7 +66,7 @@ defineProps({
                             aria-hidden="true"
                         >
                             <div
-                                class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-r from-[#ff4694] to-[#776fff] opacity-[0.45]"
+                                class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-r from-[#34d399] to-[#065f46] opacity-[0.45]"
                                 style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
                             />
                         </div>
@@ -75,7 +75,7 @@ defineProps({
                             aria-hidden="true"
                         >
                             <div
-                                class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-r from-[#ff4694] to-[#776fff] opacity-25"
+                                class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-r from-[#34d399] to-[#065f46] opacity-25"
                                 style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
                             />
                         </div>

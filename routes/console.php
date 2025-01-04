412b730260862637be7ad9ef04e9c69aeba74715
Clément Rigo <rigoclement@mydnic.be>
2025-01-04 13:07:50 +0100

    Revert "refactor(console): supprimer le fichier Kernel et réorganiser les commandes"
    This reverts commit 1b93e999cc54859c4a4c97775b09f082fbc1e67f.

--- routes/console.php
+++ routes/console.php
@@ -1,5 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schedule;
+use Illuminate\Foundation\Inspiring;
+use Illuminate\Support\Facades\Artisan;
 
-Schedule::command('gleaning:archive-old')->daily();
+Artisan::command('inspire', function () {
+    $this->comment(Inspiring::quote());
+})->purpose('Display an inspiring quote')->hourly();
